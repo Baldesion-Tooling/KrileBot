e7d32fca130c4bccde8eef63c50207e3b4ce0a92
DevJinnial (Colin) <ancient.azem@gmail.com>
2022-05-14 17:49:12 -0500

    Removed Extra Commands

--- Commands/Common.cs
+++ Commands/Common.cs
@@ -4,17 +4,5 @@ namespace KrileDotNet.Commands;
 
 public class Common : InteractionModuleBase<SocketInteractionContext>
 {
-    [SlashCommand("echo", "Echo an input")]
-    public async Task Echo(string input)
-    {
-        await RespondAsync(input);
-    }
-    
-    [SlashCommand("hello", "say hello back")]
-    public async Task Hello()
-    {
-        await RespondAsync("hi there!");
-    }
-    
     
 }
